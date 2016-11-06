3c291b11e56e04a6cf937de32fb976266f862595
Christopher Tegho <christegho@gmail.com>
2016-11-06 13:28:46 +0000

    added confusion matrix

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ from tokenize import *
 from getUniqueWords import *
 from getLexiconWeights import *
 from getSymbolicScore import *
-from getSymbolicScore import *
+from symScoreClassify import *
 from splitData import *
 
 import numpy as np
@@ -13,14 +13,14 @@ negindir = os.path.abspath('') + '\\NEG'
 posDocs = tokenize(posindir)
 negDocs = tokenize(negindir)
 
-testDocs = posDocs
+testPosDocs = posDocs
+# trainPosDocs, trainNegDocs, testPosDocs, testNegDocs = splitData(posDocs, negDocs, nfold, iteration)
 
 posLexicon = getUniqueWords(posDocs)
-
 negLexicon = getUniqueWords(negDocs)
 
 posLexiconWeights = np.array(getLexiconWeights(posLexicon, posDocs, negDocs, 1))
 negLexiconWeights = np.array(getLexiconWeights(negLexicon, negDocs, posDocs, 1))
 
 
-symbolicClassification = symScoreClassify(testDocs, posLexicon, negLexicon, posLexiconWeights, negLexiconWeights)
+posClassWeightedSS, posClassNonWeightedSS, negClassWeightedSS, negClassNonWeightedSS, tp, tn, fp, fn, weightedtp, weightedtn, weightedfp, weightedfn = symScoreClassify(testPosDocs, posLexicon, negLexicon, posLexiconWeights, negLexiconWeights, True)
--- symScoreClassify.py
+++ symScoreClassify.py
@@ -1,22 +1,69 @@
-def symScoreClassify(testDocs, posLexicon, negLexicon, posLexiconWeights, negLexiconWeights):
+from getSymbolicScore import *
+def symScoreClassify(testDocs, posLexicon, negLexicon, posLexiconWeights, negLexiconWeights, posClass):
 	#classify documents based on symbolic score and weighted symbolic score
 	posClassWeightedSS = []
 	posClassNonWeightedSS = []
 	negClassWeightedSS = []
 	negClassNonWeightedSS = []
+	tp = 0
+	tn = 0
+	fp = 0
+	fn = 0
+	weightedtp = 0
+	weightedtn = 0
+	weightedfp = 0
+	weightedfn = 0
 
 	for document in testDocs:
 		posSymbolicScores = getSymbolicScore(posLexicon, testDocs[document])
 		negSymbolicScores = getSymbolicScore(negLexicon, testDocs[document])
-		if (posSymbolicScores[0] > negSymbolicScores[0]):
+		if (posSymbolicScores[0] == negSymbolicScores[0]):
 			posClassNonWeightedSS.append(document)
+			negClassNonWeightedSS.append(document)
+
+			if (posClass):
+				tp += .5
+				fn += .5
+			else:
+				tn += .5
+				fp += .5
+
+		elif (posSymbolicScores[0] > negSymbolicScores[0]):
+			posClassNonWeightedSS.append(document)
+			if (posClass):
+				tp += 1
+			else:
+				fp += 1
 		else:
 			negClassNonWeightedSS.append(document)
+			if (posClass):
+				fn += 1
+			else:
+				tn += 1
+
+
 		posWeightedScores = np.array(posSymbolicScores[1])*posLexiconWeights
 		negWeightedScores = np.array(negSymbolicScores[1])*negLexiconWeights
-		if (sum(posWeightedScores) > sum(negWeightedScores)):
+		if (sum(posWeightedScores) == sum(negWeightedScores)):
 			posClassWeightedSS.append(document)
+			negClassWeightedSS.append(document)
+			if (posClass):
+				weightedtp += .5
+				weightedfn += .5
+			else:
+				weightedtn += .5
+				weightedfp += .5
+		elif (sum(posWeightedScores) > sum(negWeightedScores)):
+			posClassWeightedSS.append(document)
+			if (posClass):
+				weightedtp += 1
+			else:
+				weightedfp += 1
 		else:
 			negClassWeightedSS.append(document)
+			if (posClass):
+				weightedfn += 1
+			else:
+				weightedtn += 1
 
-	return posClassWeightedSS, posClassNonWeightedSS, negClassWeightedSS, negClassNonWeightedSS
+	return posClassWeightedSS, posClassNonWeightedSS, negClassWeightedSS, negClassNonWeightedSS, tp, tn, fp, fn, weightedtp, weightedtn, weightedfp, weightedfn
