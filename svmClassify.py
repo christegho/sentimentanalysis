f62029c30d85234a8bbdd6354da6679f1afd526a
Christopher Tegho <christegho@gmail.com>
2016-12-23 02:00:53 +0000

    Working on MLP

--- svmClassify.py
+++ svmClassify.py
@@ -15,6 +15,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import svm
 from sklearn.metrics import classification_report
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+
 posindir = os.path.abspath('') + '\\POS'
 negindir = os.path.abspath('') + '\\NEG'
 
@@ -26,9 +29,8 @@ iteration = 0
 trainPosDocs, trainNegDocs, testPosDocs, testNegDocs = splitDocs(posDocsLabels, negDocsLabels, nfold, iteration)
 
 
-vectorizer = CountVectorizer(ngram_range = (1,1))
-stemmer = False
-negation = False
+stemmer = True
+negation = True
 #vectorizer = TfidfVectorizer(min_df=5,max_df = 0.8, sublinear_tf=True,use_idf=True)
 
 train_data = []
@@ -65,28 +67,10 @@ for fname in testNegDocs:
         test_data.append(' '.join(tokens))
         test_labels.append(0)
 
-
+vectorizer = CountVectorizer(ngram_range = (2,2))
 train_vectors = vectorizer.fit_transform(train_data)
 test_vectors = vectorizer.transform(test_data)
 
-classifier_rbf = svm.SVC()
-t0 = time.time()
-classifier_rbf.fit(train_vectors, train_labels)
-t1 = time.time()
-prediction_rbf = classifier_rbf.predict(test_vectors)
-t2 = time.time()
-time_rbf_train = t1-t0
-time_rbf_predict = t2-t1
-
-# Perform classification with SVM, kernel=linear
-classifier_linear = svm.SVC(kernel='linear')
-t0 = time.time()
-classifier_linear.fit(train_vectors, train_labels)
-t1 = time.time()
-prediction_linear = classifier_linear.predict(test_vectors)
-t2 = time.time()
-time_linear_train = t1-t0
-time_linear_predict = t2-t1
 
 # Perform classification with SVM, kernel=linear
 classifier_liblinear = svm.LinearSVC()
@@ -98,20 +82,41 @@ t2 = time.time()
 time_liblinear_train = t1-t0
 time_liblinear_predict = t2-t1
 
-tp = sum(prediction_liblinear[:99])
-fp = sum(prediction_liblinear[99:])
-tn = 99-fp
-fn = 99-tp
+tpSVM = sum(prediction_liblinear[:99])
+fpSVM = sum(prediction_liblinear[99:])
+tnSVM = 99-fpSVM
+fnSVM = 99-tpSVM
+
+classifier_mnb = MultinomialNB()
+classifier_mnb.fit(train_vectors, train_labels)
+prediction_mnb = classifier_mnb.predict(test_vectors)
+
+tpNB = sum(prediction_mnb[:99])
+fpNB =  sum(prediction_mnb[99:])
+tnNB = 99-fpNB
+fnNB = 99-tpNB
+
+from sklearn.neural_network import MLPClassifier
+
+#training and fitting of the MLP
+clf = MLPClassifier(solver='lbfgs', alpha=.8, hidden_layer_sizes=(5, 4), random_state=1)
+clf.fit(train_vectors, train_labels) 
+
+#predict labels
+yTest = clf.predict(test_vectors) 
+
+tp = sum(yTest[:99])
+tn = sum(yTest[99:])    
 
 
-
-# Print results in a nice table
-print("Results for SVC(kernel=rbf)")
-print("Training time: %fs; Prediction time: %fs" % (time_rbf_train, time_rbf_predict))
-print(classification_report(test_labels, prediction_rbf))
-print("Results for SVC(kernel=linear)")
-print("Training time: %fs; Prediction time: %fs" % (time_linear_train, time_linear_predict))
-print(classification_report(test_labels, prediction_linear))
-print("Results for LinearSVC()")
+print("Results for SVM")
 print("Training time: %fs; Prediction time: %fs" % (time_liblinear_train, time_liblinear_predict))
 print(classification_report(test_labels, prediction_liblinear))
+print "TP and TN"
+print tpSVM, tnSVM
+print("Results for MNB")
+print(classification_report(test_labels, prediction_mnb))
+print "TP and TN"
+print tpNB, tnNB
+print("Results for MLP")
+print(classification_report(test_labels, yTest))
